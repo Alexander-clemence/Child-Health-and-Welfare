c5c3ca0d9f344a6fe6b9046bf54cc645db9c4f23
Alexander-clemence <51910485+Alexander-clemence@users.noreply.github.com>
2024-04-26 01:29:08 +0300

    contact us form updated

--- Front-End/chwf/src/app/Contact/contactform.tsx
+++ Front-End/chwf/src/app/Contact/contactform.tsx
@@ -1,10 +1,34 @@
 'use client'
 import React from 'react'
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
 
-const contactform = () => {
+
+
+
+import styles from './contac.module.css'
+
+const Contactform = () => {
   return (
-    <div>contactform</div>
+    <>
+    <div className={styles.title}>
+      <Label htmlFor="title">Send Us a Message</Label>
+    </div><br/>
+    <div className={styles.input}>
+      <Label htmlFor="Name">Name</Label> 
+      <Input type="text" id="name" placeholder="Enter Your Name Please"/><br/>
+      <Label htmlFor="email">Email</Label>
+      <Input type="email" id="email" placeholder="Enter Your Email"/><br/>
+      <Label htmlFor="Subject">Subject</Label>
+      <Input type="text" id="subject" placeholder="Subject"/><br/>
+      <Label htmlFor="message">Message</Label>
+      <Textarea placeholder="Type your message here." /><br/>
+      <Button>Send</Button>
+    </div>
+    </>
   )
 }
 
-export default contactform
+export default Contactform
--- Front-End/chwf/src/app/Contact/page.tsx
+++ Front-End/chwf/src/app/Contact/page.tsx
@@ -1,12 +1,16 @@
 'use client'
 import React from 'react'
 import styles from './about.module.css'
+import Contactform from './contactform'
 
 const ContactUs = () => {
   return (
     <div className={styles.contact}>
       <div className={styles.container}>
-  hi
+  <div className={styles.text}>At [Your Foundation Name], we are here to help! We understand that you may have questions, want to learn more about our programs, or even share your story.  We encourage you to reach out to us using the methods below.
+
+Phone: [Phone Number] (Our phone lines are open from Hours of Operation)</div>
+  <div className={styles.form}><Contactform/></div>
         </div> 
     </div>
    
